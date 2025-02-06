0d78394853a9396257ce5da4376d52a3c63f22da
fplmarques <fplmarques@gmail.com>
2025-02-06 16:49:48 -0300

    Added TSV files for ICDs

---
+++ databases/json2tsv.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+# python3 print_json.py aetiology.json > aetiology.tsv
+python3 print_json.py antimicrobial_resistance.json > antimicrobial_resistance.tsv
+python3 print_json.py infectious_agents.json > infectious_agents.tsv
+python3 print_json.py infectious_and_vectorborne.json > infectious_and_vectorborne.tsv
+python3 print_json.py opioids.json > opioids.tsv
+python3 print_json.py vaccines.json > vaccines.tsv
